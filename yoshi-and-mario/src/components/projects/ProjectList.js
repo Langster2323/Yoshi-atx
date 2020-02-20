ff9d58d19ae5c3840c49bb50a1e77b8e409302ea
Langster2323 <langahkeem@gmail.com>
2020-02-19 20:50:55 -0600

    Deveoped dashboard and project list

--- yoshi-and-mario/src/components/projects/ProjectList.js
+++ yoshi-and-mario/src/components/projects/ProjectList.js
@@ -10,9 +10,6 @@ const ProjectList = () => {
                     <p className="grey-text">3rd September, 2am</p>
                 </div>
             </div>
-        </div>
-
-        <div className="project-list section">
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
@@ -20,8 +17,6 @@ const ProjectList = () => {
                     <p className="grey-text">3rd September, 2am</p>
                 </div>
             </div>
-        </div>
-        <div className="project-list section">
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
@@ -29,17 +24,6 @@ const ProjectList = () => {
                     <p className="grey-text">3rd September, 2am</p>
                 </div>
             </div>
-        </div>
-        <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-        </div>
-        <div className="project-list section">
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">Project Title</span>
